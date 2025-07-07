55000b1555825b246b80aff8b54f48e0424668da
svinson1121 <root@NODE-SMSC.devops.mnc435.mcc311.3gppnetwork.org>
2025-07-07 20:04:04 +0000

    added Eir add and edit menus - More work is needed

--- src/components/eir/add.tsx
+++ src/components/eir/add.tsx
@@ -42,7 +42,7 @@ const EirAddItem = (props: { open: ReturnType<typeof Boolean>, handleClose: Retu
 
   const handleSave = () => {
     if (edit) {
-      EirApi.update(data.apn_id, state).then(() => {
+      EirApi.update(data.eir_id, state).then(() => {
         handleClose();
       })
     } else {
@@ -65,7 +65,7 @@ const EirAddItem = (props: { open: ReturnType<typeof Boolean>, handleClose: Retu
        aria-describedby="modal-modal-description"
      >
        <Box sx={style}>
-        <h3>{i18n.t('apn.headAdd', {"mode": (edit?i18n.t('generic.edit'):i18n.t('generic.add'))})}</h3>
+        <h3>{i18n.t('eir.headAdd', {"mode": (edit?i18n.t('generic.edit'):i18n.t('generic.add'))})}</h3>
         <Box
           component="form"
           noValidate
@@ -77,189 +77,47 @@ const EirAddItem = (props: { open: ReturnType<typeof Boolean>, handleClose: Retu
                 required
                 value={state.eir}
                 onChange={handleChange}
-                id="apn"
-                label={i18n.t('inputFields.header.apn')}
-              >{i18n.t('inputFields.desc.apn')}</InputField>
+                id="imei"
+                label={i18n.t('inputFields.header.imei')}
+              >{i18n.t('inputFields.desc.imei')}</InputField>
             </Grid>
-            <Grid item xs={4}>
-              <SelectField
-                value={state.nbiot}
-                onChange={handleChange}
-                id="nbiot"
-                label={i18n.t('inputFields.header.nbiot')}
-                helper={i18n.t('inputFields.desc.nbiot')}
-              >
-                <MenuItem value={true}>{i18n.t('generic.yes')}</MenuItem>
-                <MenuItem value={false}>{i18n.t('generic.no')}</MenuItem>
-              </SelectField>
-            </Grid>
-            <Grid item xs={12}><h3>{i18n.t('apn.gatewayHead')}</h3></Grid>
-            <Grid item xs={6}>
+            <Grid item xs={5}>
               <InputField
                 required
-                value={state.pgw_address}
+                value={state.imsi}
                 onChange={handleChange}
-                id="pgw_address"
-                label={i18n.t('inputFields.header.pgw')}
+                id="imsi"
+                label={i18n.t('inputFields.header.imsi')}
               >
-                {i18n.t('inputFields.desc.pgw')}
+                {i18n.t('inputFields.desc.imsi')}
               </InputField>
             </Grid>
-            <Grid item xs={6}>
-              <InputField
-                required
-                value={state.sgw_address}
-                onChange={handleChange}
-                id="sgw_address"
-                label={i18n.t('inputFields.header.sgw')}
-              >
-                {i18n.t('inputFields.desc.sgw')}
-              </InputField>
-            </Grid>
-            <Grid item xs={12}><h3>{i18n.t('apn.chargingHead')}</h3></Grid>
-            <Grid item xs={6}>
-              <InputField
-                value={state.charging_characteristics}
-                onChange={handleChange}
-                id="charging_characteristics"
-                label={i18n.t('inputFields.header.chargingCharacteristics')}
-              >
-                {i18n.t('inputFields.desc.chargingCharacteristics')}
-              </InputField>
-            </Grid>
-            <Grid item xs={6}>
-              <InputField
-                value={state.charging_rule_list}
-                onChange={handleChange}
-                id="charging_rule_list"
-                label="Charging rule list"
-              >
-                Comma seperated list of charging rules
-              </InputField>
-            </Grid>
-            <Grid item xs={12}><h3>{i18n.t('apn.qosHead')}</h3></Grid>
+            <Grid item xs={12}><h3>{i18n.t('eir.mode')}</h3></Grid>
             <Grid item xs={3}>
-              <SelectField
-                value={state.qci}
-                onChange={handleChange}
-                id="qci"
-                label={i18n.t('inputFields.header.qci')}
-                helper={i18n.t('inputFields.desc.qci')}
-              >
-                {Array.from(Array(9), (e, i) => (<MenuItem key={i+1+e} value={i+1}>{i+1}</MenuItem>))}
-              </SelectField>
+	     <SelectField
+  	     value={state.regex_mode}
+  	     onChange={handleChange}
+             id="regex_mode"
+             label={i18n.t('inputFields.header.regex_mode')}
+             helper={i18n.t('inputFields.desc.regex_mode')}
+	     >
+ 	     <MenuItem value={0}>exact matching</MenuItem>
+             <MenuItem value={1}>loose matching</MenuItem>
+            </SelectField>
             </Grid>
             <Grid item xs={4}>
-              <InputField
-                required
-                value={state.apn_ambr_ul}
-                onChange={handleChange}
-                id="apn_ambr_ul"
-                label={i18n.t('inputFields.header.ambr_ul')}
-              >{i18n.t('inputFields.desc.ambr_ul')} <NetworkBandwidthFormatter data={state.apn_ambr_ul} /></InputField>
+		<SelectField
+ 		 value={state.match_response_code}
+ 		 onChange={handleChange}
+ 		 id="match_response_code"
+ 		 label={i18n.t('inputFields.header.match_response_code')}
+ 		 helper={i18n.t('inputFields.desc.match_response_code')}
+		>
+ 		 <MenuItem value={0}>Whitelist</MenuItem>
+ 		 <MenuItem value={1}>Blacklist</MenuItem>
+ 		 <MenuItem value={2}>Greylist</MenuItem>
+		</SelectField>
             </Grid>
-            <Grid item xs={4}>
-              <InputField
-                required
-                value={state.apn_ambr_dl}
-                onChange={handleChange}
-                id="apn_ambr_dl"
-                label={i18n.t('inputFields.header.ambr_dl')}
-              >{i18n.t('inputFields.desc.ambr_dl')} <NetworkBandwidthFormatter data={state.apn_ambr_dl} /></InputField>
-            </Grid>
-            <Grid item xs={12}><h4>{i18n.t('apn.arpHead')}</h4></Grid>
-            <Grid item xs={4}>
-              <SelectField
-                value={state.arp_priority}
-                onChange={handleChange}
-                id="arp_priority"
-                label={i18n.t('inputFields.header.arpPriority')}
-                helper={i18n.t('inputFields.desc.arpPriority')}
-                >
-                  {Array.from(Array(15), (e, i) => (<MenuItem key={i+1+e} value={i+1}>{i+1}</MenuItem>))}
-              </SelectField>
-            </Grid>
-            <Grid item xs={4}>
-              <SelectField
-                value={state.arp_preemption_capability}
-                onChange={handleChange}
-                id="arp_preemption_capability"
-                label={i18n.t('inputFields.header.arpPreemptionCapability')}
-                helper={i18n.t('inputFields.desc.arpPreemptionCapability')}
-                >
-                  <MenuItem value={true}>{i18n.t('generic.yes')}</MenuItem>
-                  <MenuItem value={false}>{i18n.t('generic.no')}</MenuItem>
-              </SelectField>
-            </Grid>
-            <Grid item xs={4}>
-              <SelectField
-                value={state.arp_preemption_vulnerability}
-                onChange={handleChange}
-                id="arp_preemption_vulnerability"
-                label={i18n.t('inputFields.header.arpPreemptionVulnerability')}
-                helper={i18n.t('inputFields.desc.arpPreemptionVulnerability')}
-                >
-                  <MenuItem value={true}>{i18n.t('generic.yes')}</MenuItem>
-                  <MenuItem value={false}>{i18n.t('generic.no')}</MenuItem>
-              </SelectField>
-            </Grid>
-            {state.nbiot && <> 
-            <Grid item xs={12}><h3>{i18n.t('apn.nbiotHead')}</h3></Grid>
-            <Grid item xs={4}>
-              <InputField
-                required
-                value={state.nidd_scef_id}
-                onChange={handleChange}
-                id="nidd_scef_id"
-                label={i18n.t('inputFields.header.scefId')}
-              >{i18n.t('inputFields.desc.scefId')}</InputField>
-            </Grid>
-            <Grid item xs={4}>
-              <InputField
-                required
-                value={state.nidd_scef_realm}
-                onChange={handleChange}
-                id="nidd_scef_realm"
-                label={i18n.t('inputFields.header.scefRealm')}
-              >{i18n.t('inputFields.desc.scefRealm')}</InputField>
-            </Grid>
-            <Grid item xs={4}>
-              <SelectField
-                value={state.nidd_mechanism}
-                onChange={handleChange}
-                id="nidd_mechanism"
-                label={i18n.t('inputFields.header.niddMechanism')}
-                helper={i18n.t('inputFields.desc.niddMechanism')}
-                >
-                  <MenuItem value={0}>SGi-BASED-DATA-DELIVERY</MenuItem>
-                  <MenuItem value={1}>SCEF-BASED-DATA-DELIVERY</MenuItem>
-              </SelectField>
-            </Grid>
-            <Grid item xs={4}>
-              <SelectField
-                value={state.nidd_rds}
-                onChange={handleChange}
-                id="nidd_rds"
-                label={i18n.t('inputFields.header.niddRds')}
-                helper={i18n.t('inputFields.desc.niddRds')}
-                >
-                  <MenuItem value={0}>{i18n.t('generic.disabled')}</MenuItem>
-                  <MenuItem value={1}>{i18n.t('generic.enabled')}</MenuItem>
-              </SelectField>
-            </Grid>
-            <Grid item xs={4}>
-              <SelectField
-                value={state.nidd_preferred_data_mode}
-                onChange={handleChange}
-                id="nidd_preferred_data_mode"
-                label={i18n.t('inputFields.header.niddPreferredDataMode')}
-                helper={i18n.t('inputFields.desc.niddPreferredDataMode')}
-                >
-                  <MenuItem value={0}>{i18n.t('inputFields.options.niddPreferredDataMode.0')}</MenuItem>
-                  <MenuItem value={1}>{i18n.t('inputFields.options.niddPreferredDataMode.1')}</MenuItem>
-              </SelectField>
-            </Grid>
-          </>}
           </Grid>
          </Box>
          <SaveButtons onClickClose={handleLocalClose} onClickSave={handleSave} />
